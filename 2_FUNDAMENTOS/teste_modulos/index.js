52c688a8cffcae30de4308c05c9e048ce92a09db
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-10-16 19:33:58 -0300

    modulo interno

---
+++ 2_FUNDAMENTOS/teste_modulos/index.js
@@ -0,0 +1,5 @@
+const model = require('./modulo');
+
+model.any('Olá mundo!');
+model.any('Olá mundo 2!');
+model.any('Olá mundo 3!');
---
+++ 2_FUNDAMENTOS/teste_modulos/modulo.js
@@ -0,0 +1,6 @@
+module.exports = {
+    any(a) {
+        console.log(a)
+    }
+}
+
