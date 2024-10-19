dcd2188379409153c466d19334654ed38b5368c8
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-10-19 10:33:41 -0300

    mais sobre o console log

---
+++ 2_FUNDAMENTOS/7_explorando_o_console/index.js
@@ -0,0 +1,15 @@
+const x = 18;
+const y = 'joao'
+const z = [1, 2, 3]
+
+console.log(x, y, z);
+
+console.log('o nome dele e %s, e ele tem %d anos', y, x);
+
+
+
+
+
+setTimeout(() => {
+    console.clear();
+}, 5000)
