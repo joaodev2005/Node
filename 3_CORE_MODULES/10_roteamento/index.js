1b20187a6e9db24b8cfaf649af7b3e47d966055b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-11-04 19:24:31 -0300

    roteamento

---
+++ 3_CORE_MODULES/10_roteamento/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    const q = url.parse(req.url, true);
+    const name = q.pathname.substring(1);
+
+    if (name.includes('.html')) {
+       if(fs.existsSync(name)) {
+        fs.readFile(name, 'utf8', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        })
+       } else {
+        fs.readFile('404.html', 'utf8', (err, data) => {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        })
+       }
+    } 
+});
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+})
