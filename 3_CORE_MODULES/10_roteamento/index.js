a7e1ebf670dee654f131df386fc402c4a6c20a2b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-11-04 19:34:54 -0300

    verificando dados

--- 3_CORE_MODULES/10_roteamento/index.js
+++ 3_CORE_MODULES/10_roteamento/index.js
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             res.write(data);
             return res.end();
-        })
+        }) 
        }
     } 
 });
