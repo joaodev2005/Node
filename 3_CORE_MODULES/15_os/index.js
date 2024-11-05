1e061ed180f4680f7d9c39717fc2d02b1b2d781e
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-11-05 19:41:10 -0300

    os ( SYSTEM OPERACIONAL )

---
+++ 3_CORE_MODULES/15_os/index.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.release());
+console.log(os.cpus());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.homedir());
