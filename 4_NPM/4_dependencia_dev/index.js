69eff539cddd3cfe89eb8d161fe3ba0bff71ae8c
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-11-07 19:57:54 -0300

    dependencias dev

--- 4_NPM/4_dependencia_dev/index.js
+++ 4_NPM/4_dependencia_dev/index.js
@@ -1,8 +1,9 @@
-const _ = require('lodash')
+import _ from "lodash"
+import chalk from  'chalk'
 
 const a = [1, 2, 3, 4, 5]
 const b = [2, 4, 6, 7, 8]
 
 const diff = _.difference(a, b)
 
-console.log(diff)
+console.log(chalk.red.bold(diff))
