b69f885cf443aff48d1d542172a720cc363f6cab
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-11-20 11:00:37 -0300

    feat: withdraw an amount from user account

--- 5_Accounts/index.js
+++ 5_Accounts/index.js
@@ -31,6 +31,7 @@ function operation() {
                     deposit();
                     break;
                 case 'Sacar':
+                    withdraw();
                     break;
                 case 'Consultar saldo':
                     getAccountBalance();
@@ -193,4 +194,65 @@ function getAccountBalance() {
             operation();
 
         }).catch(err => console.log(err));
+}
+
+function withdraw() {
+    inquirer
+        .prompt([
+            {
+                name: 'accountName',
+                message: 'Qual a conta deseja sacar?'
+            }
+        ])
+        .then((answer) => {
+            const accountName = answer['accountName'];
+
+            if(!checkAccount(accountName)) {
+                return withdraw();
+            }
+
+            inquirer.prompt([
+                {
+                    name: 'amount',
+                    message: 'Quanto deseja sacar?'
+                }
+            ])
+            .then((answer) => {
+                const amount = answer['amount'];
+
+                removeAmount(accountName, amount);
+
+            }).catch(err => console.log(err));
+
+        })
+        .catch(err => console.log(err));
+}
+
+function removeAmount(accountName, amount) {
+
+    const accountData = getAccount(accountName);
+
+    if (!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+        return withdraw()
+    }
+
+    if (accountData.balance < amount) {
+        console.log(chalk.bgRed.black('Saldo insuficiente'));
+        return withdraw();
+    }
+
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err);
+        }
+    )
+
+    console.log(chalk.green(`Foi sacado ${amount} da sua conta ${accountName}`));
+
+    operation();
 }
