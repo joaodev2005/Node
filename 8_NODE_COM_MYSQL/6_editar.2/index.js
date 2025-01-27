6f116fed8043507f8bc3f4736521ed11eb37f0ea
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-01-27 19:32:38 -0300

    editar.2

--- 8_NODE_COM_MYSQL/6_editar.2/index.js
+++ 8_NODE_COM_MYSQL/6_editar.2/index.js
@@ -81,6 +81,22 @@ app.get('/books/edit/:id', (req, res) => {
 
 })
 
+app.post('/books/updatebook', (req, res) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`;
+
+    conn.query(sql, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/books');
+    })
+})
+
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
