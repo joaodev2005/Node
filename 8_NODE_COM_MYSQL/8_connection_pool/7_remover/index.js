f4152b921c06adb5af5753f1b25a0a8e752ac046
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-01-28 19:31:28 -0300

    querry

---
+++ 8_NODE_COM_MYSQL/8_connection_pool/7_remover/index.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const pool = require('./db/conn');
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json());
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+    conn.query(sql, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/books');
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books';
+
+    conn.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const books = data;
+        res.render('books', { books });
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+    pool.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const book = data[0];
+        res.render('book', { book });
+    })
+
+})
+
+app.get('/books/edit/:id', (req, res) => {
+    
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+    pool.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const book = data[0];
+        res.render('editbook', { book });
+    })
+
+})
+
+app.post('/books/updatebook', (req, res) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`;
+
+    StylePropertyMapReadOnly.query(sql, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/books');
+    })
+})
+
+app.post('/books/remove/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    const sql = `DELETE FROM books WHERE id = ${id}`;
+
+    pool.query(sql, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/books');
+    })
+})
+
+// const conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'nodemysql'
+// });
+
+// conn.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('Banco de dados conectado com sucesso');
+
+//     app.listen(3000, () => {
+//         console.log('Servidor rodando na porta 3000');
+//     })
+// });
+
