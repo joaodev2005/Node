e6739bffe676cff499817c83f1e220fc3f07d00e
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-03-04 19:38:47 -0300

    salvando dados no banco

--- 10_MVC/1_estrutura/models/Task.js
+++ 10_MVC/1_estrutura/models/Task.js
@@ -5,15 +5,15 @@ const db = require('../db/conn');
 const Task = db.define('Task', {
     title: {
         type: DataTypes.STRING,
-        required: true
+        allowNull: true
     },
     description: {
         type: DataTypes.STRING,
-        required: true
+        allowNull: true
     },
     done: {
         type: DataTypes.BOOLEAN,
-        required: true
+        allowNull: true
     },
 });
 
