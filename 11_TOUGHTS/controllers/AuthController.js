3a3b747727ae089eff53e0f8746414a18597b512
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-03-25 19:01:17 -0300

    new features

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -1,58 +1,160 @@
-const User = require('../models/User');
+// const User = require('../models/User');
 
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-module.exports = class AuthController {
-    static login(req, res) {
-        res.render('auth/login');
-    }
+// module.exports = class AuthController {
+//     static login(req, res) {
+//         res.render('auth/login');
+//     }
 
-    static register(req, res) {
-        res.render('auth/register');
-    }
+//     static register(req, res) {
+//         res.render('auth/register');
+//     }
 
-    static async registerPost(req, res) {
+//     static async registerPost(req, res) {
 
-        const { name, email, password, confirmpassword } = req.body;
+//         const { name, email, password, confirmpassword } = req.body;
 
-        if (password != confirmpassword) {
-            req.flash('message', 'Senhas não conferem');
-            res.render('auth/register');
+//         if (password != confirmpassword) {
+//             req.flash('message', 'Senhas não conferem');
+//             res.render('auth/register');
 
-            return;
-        }
+//             return;
+//         }
 
-        const checkIfUserExists = await User.findOne({ where: { email: email } });
+//         const checkIfUserExists = await User.findOne({ where: { email: email } });
 
-        if (checkIfUserExists) {
-            req.flash('message', 'Email em uso');
-            res.render('auth/register');
+//         if (checkIfUserExists) {
+//             req.flash('message', 'Email em uso');
+//             res.render('auth/register');
 
-            return;
-        }
+//             return;
+//         }
 
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(password, salt);
+//         const salt = bcrypt.genSaltSync(10);
+//         const hashedPassword = bcrypt.hashSync(password, salt);
 
-        const user = {
-            name,
-            email,
-            password: hashedPassword
-        }
+//         const user = {
+//             name,
+//             email,
+//             password: hashedPassword
+//         }
 
-        try {
-            const createdUser = await User.create(user);
+//         try {
+//             const createdUser = await User.create(user);
 
-            req.session.userId = createdUser.id;
+//             req.session.userId = createdUser.id;
 
-            req.flash('message', 'User created successfully');
+//             req.flash('message', 'User created successfully');
 
-            req.session.save(() => {
-                res.redirect('/');
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}
+//             req.session.save(() => {
+//                 res.redirect('/');
+//             });
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+// }
     
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
+module.exports = class UserController {
+  static login(req, res) {
+    res.render('auth/login')
+  }
+
+  static async loginPost(req, res) {
+    const { email, password } = req.body
+
+    // find user
+    const user = await User.findOne({ where: { email: email } })
+
+    if (!user) {
+      res.render('auth/login', {
+        message: 'Usuário não encontrado!',
+      })
+
+      return
+    }
+
+    // compare password
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+
+    if (!passwordMatch) {
+      res.render('auth/login', {
+        message: 'Senha inválida!',
+      })
+
+      return
+    }
+
+    // auth user
+    req.session.userid = user.id
+
+    req.flash('message', 'Login realizado com sucesso!')
+
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  }
+
+  static register(req, res) {
+    res.render('auth/register')
+  }
+
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body
+
+    // passwords match validation
+    if (password != confirmpassword) {
+      req.flash('message', 'As senhas não conferem, tente novamente!')
+      res.render('auth/register')
+
+      return
+    }
+
+    // email validation
+    const checkIfUserExists = await User.findOne({ where: { email: email } })
+
+    if (checkIfUserExists) {
+      req.flash('message', 'O e-mail já está em uso!')
+      res.render('auth/register')
+
+      return
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    }
+
+    User.create(user)
+      .then((user) => {
+        // initialize session
+        req.session.userid = user.id
+
+        // console.log('salvou dado')
+        // console.log(req.session.userid)
+
+        req.session.userid = user.id
+
+        req.flash('message', 'Cadastro realizado com sucesso!')
+
+        req.session.save(() => {
+          res.redirect('/')
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  static logout(req, res) {
+    req.session.destroy()
+    res.redirect('/login')
+  }
+}
--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -1,80 +1,162 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-const flash = require('connect-flash');
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+// const flash = require('connect-flash');
+
+// const app = express();
+
+// const conn = require('./db/conn');
+
+// const Tought = require('./models/Tought');
+// const User = require('./models/User');
+
+// const toughtRoutes = require('./routes/toughtRoutes');
+// const authRoutes = require('./routes/authRoutes');
+
+// const ToughtsController = require('./controllers/ToughtController');
+
+// app.engine('handlebars', exphbs.engine());
+// app.set('view engine', 'handlebars');
+
+// app.use(
+//     express.urlencoded({
+//         extended: true
+//     })
+// )
+
+// app.use(express.json());
+
+// app.use(
+//     session({
+//         name: 'session',
+//         secret: 'nosso_secret',
+//         resave: false,
+//         saveUninitialized: false,
+//         store: new FileStore({
+//             logFn: function () { },
+//             path: require('path').join(require('os').tmpdir(), 'sessions')
+//         }),
+//         cookie: {
+//             secure: false,
+//             maxAge: 3600000,
+//             httpOnly: true
+//         },
+//         cookie: {
+//             secure: false,
+//             maxAge: 3600000,
+//             expires: new Date(Date.now() + 3600000),
+//             httpOnly: true
+//         }
+//     })
+// )
+
+// app.use(flash());
+
+// app.use((req, res, next) => {
+//     res.locals.messages = req.flash();
+//     next();
+// });
+
+// app.use(express.static('public'));
+
+// app.use((req, res, next) => {
+
+//     console.log(req.session.userId);
+
+//     if (req.session.userId) {
+//         res.locals.session = req.session;
+//     }
+//     next();
+// })
+
+// app.use('/toughts', toughtRoutes);
+// app.use('/', authRoutes);
+
+// app.get('/', ToughtsController.showToughts);
+
+
+// conn.sync()
+// .then(() => {
+//     app.listen(3000, () => {
+//         console.log('Servidor rodando na porta 3000');
+//     })
+// }).catch((err) => console.log(err));
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
 const app = express();
 
-const conn = require('./db/conn');
+const conn = require("./db/conn");
 
-const Tought = require('./models/Tought');
-const User = require('./models/User');
+// Models
+const Tought = require("./models/Tought");
 
-const toughtRoutes = require('./routes/toughtRoutes');
-const authRoutes = require('./routes/authRoutes');
+// routes
+const toughtRoutes = require("./routes/toughtRoutes");
+const authRoutes = require("./routes/authRoutes");
+const ToughController = require("./controllers/ToughtController");
 
-const ToughtsController = require('./controllers/ToughtController');
-
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.json());
 
+//session middleware
 app.use(
-    session({
-        name: 'session',
-        secret: 'nosso_secret',
-        resave: false,
-        saveUninitialized: false,
-        store: new FileStore({
-            logFn: function () { },
-            path: require('path').join(require('os').tmpdir(), 'sessions')
-        }),
-        cookie: {
-            secure: false,
-            maxAge: 3600000,
-            httpOnly: true
-        },
-        cookie: {
-            secure: false,
-            maxAge: 3600000,
-            expires: new Date(Date.now() + 3600000),
-            httpOnly: true
-        }
-    })
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
 )
 
+// flash messages
 app.use(flash());
 
+app.use(express.static("public"));
+
+// set session to res
 app.use((req, res, next) => {
-    res.locals.messages = req.flash();
-    next();
+  // console.log(req.session)
+  console.log(req.session.userid);
+
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
 });
 
-app.use(express.static('public'));
+app.use("/toughts", toughtRoutes);
+app.use("/", authRoutes);
 
-app.use((req, res, next) => {
-    if (req.session.userId) {
-        res.locals.session = req.session;
-    }
-    next();
-})
+app.get("/", ToughController.showToughts);
 
-app.use('/toughts', toughtRoutes);
-app.use('/', authRoutes);
-
-app.get('/', ToughtsController.showToughts);
-
-conn.sync()
-.then(() => {
-    app.listen(3000, () => {
-        console.log('Servidor rodando na porta 3000');
-    })
-}).catch((err) => console.log(err));
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err)); 
