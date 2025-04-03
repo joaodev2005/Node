c9064cbac8bdc1a28820b311e09ea5e0b0e135e1
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-04-03 18:22:14 -0300

    feat: add functionality to remove thoughts

--- 11_TOUGHTS/controllers/toughtController.js
+++ 11_TOUGHTS/controllers/toughtController.js
@@ -115,6 +115,26 @@ module.exports = class ToughtController {
             res.redirect("/toughts/dashboard");
         }
     }
+
+    static async removeTought(req, res) {
+
+        const id = req.body.id;
+        const UserId = req.session.userid;
+
+       
+
+        try {
+            await Tought.destroy({ where: { id: id, UserId: UserId } });
+
+            req.flash('message', 'Pensamento removido com sucesso!');
+
+            req.session.save(() => {    
+                res.redirect('/toughts/dashboard');
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
     
 }
 
