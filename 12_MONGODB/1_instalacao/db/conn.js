5ab03f85d99414bcf9446d24d6caf091b1665813
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-04-10 18:57:54 -0300

    chore: setup MongoDB connection in Node.js

---
+++ 12_MONGODB/1_instalacao/db/conn.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb://localhost:27017/testemongodb";
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+
+        await client.connect();
+        console.log("Conectado ao MongoDB!");
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+run()
+
+module.exports = client
---
+++ 12_MONGODB/1_instalacao/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const conn = require('./db/conn.js');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({ extended: true })
+)
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000!')
+})
