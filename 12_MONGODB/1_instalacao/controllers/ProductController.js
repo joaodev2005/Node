56e130282a15824038d1f8baad613db3150a1476
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-04-14 20:40:12 -0300

    feat: fetch and display data from database on screen

--- 12_MONGODB/1_instalacao/controllers/ProductController.js
+++ 12_MONGODB/1_instalacao/controllers/ProductController.js
@@ -1,7 +1,26 @@
 const Product = require('../models/Product');
 
 module.exports= class ProductController {
-    static showProducts(req, res) {
-        res.render('products/all');
+    static async showProducts(req, res) {
+
+        const products = await Product.getProducts();
+
+        res.render('products/all', { products });
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create');
+    }
+
+    static createProductPost(req, res) {
+        const name = req.body.name;
+        const image = req.body.image;
+        const description = req.body.description;
+        const price = req.body.price;
+
+        const product = new Product(name, image, price, description);
+        product.save().then(() => {
+            res.redirect('/products');    
+        }).catch(err => console.log(err));
     }
 }
