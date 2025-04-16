34a3291e13faab14cf942d151e299e8961834872
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-04-16 18:55:32 -0300

    refactor(app): migrate application to use Mongoose for data modeling and database operations

--- 13_MONGOSE/1_instalacao/controllers/ProductController.js
+++ 13_MONGOSE/1_instalacao/controllers/ProductController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product');
 module.exports= class ProductController {
     static async showProducts(req, res) {
 
-        const products = await Product.getProducts();
+        const products = await Product.find().lean()
 
         res.render('products/all', { products });
     }
@@ -12,54 +12,55 @@ module.exports= class ProductController {
         res.render('products/create');
     }
 
-    static createProductPost(req, res) {
+    static async createProductPost(req, res) {
         const name = req.body.name;
         const image = req.body.image;
         const description = req.body.description;
         const price = req.body.price;
 
-        const product = new Product(name, image, price, description);
-        product.save().then(() => {
+        const product = new Product({name, image, price, description});
+        
+        await product.save().then(() => {
             res.redirect('/products');    
         }).catch(err => console.log(err));
     }
 
     static async getProduct(req, res) {
         const id = req.params.id;
-        const product = await Product.getProductById(id);
+        const product = await Product.findById(id).lean();
 
         res.render('products/product', { product });
     }
 
-    static async removeProduct(req, res) {
-        const id = req.params.id;
+    // static async removeProduct(req, res) {
+    //     const id = req.params.id;
 
-        // await Product.remove(id);
+    //     // await Product.remove(id);
 
-        await Product.removeProductById(id);
+    //     await Product.removeProductById(id);
 
-        res.redirect('/products');
-    }
+    //     res.redirect('/products');
+    // }
 
-    static async editProduct(req, res) {
-        const id = req.params.id;
-        const product = await Product.getProductById(id);
+    // static async editProduct(req, res) {
+    //     const id = req.params.id;
+    //     const product = await Product.getProductById(id);
 
-        res.render('products/edit', { product });
-    }
+    //     res.render('products/edit', { product });
+    // }
 
-    static async editProductPost(req, res) {
+    // static async editProductPost(req, res) {
 
-        const id = req.body.id;
-        const name = req.body.name;
-        const image = req.body.image;
-        const price = req.body.price;
-        const description = req.body.description;
+    //     const id = req.body.id;
+    //     const name = req.body.name;
+    //     const image = req.body.image;
+    //     const price = req.body.price;
+    //     const description = req.body.description;
 
-        const product = new Product(name, image, price, description);
+    //     const product = new Product(name, image, price, description);
 
-        await product.updateProduct(id);
+    //     await product.updateProduct(id);
 
-        res.redirect('/products');
-    }
+    //     res.redirect('/products');
+    // }
 }
--- 13_MONGOSE/1_instalacao/db/conn.js
+++ 13_MONGOSE/1_instalacao/db/conn.js
@@ -1,20 +1,11 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/testemongodb";
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/testemongoose');
 
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-
-        await client.connect();
-        console.log("Conectado ao MongoDB!");
-
-    } catch (error) {
-        console.error(error);
-    }
+    console.log('Conectado ao MongoDB!');
 }
 
-run()
+main().catch(err => console.log(err));
 
-module.exports = client
+module.exports = mongoose;
